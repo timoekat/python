b6d05ed1d73319ddac340610c4c54b596920cf19
Ekaterina <ekaterina.timofeeva.qa@gmail.com>
2025-10-31 08:48:53 +0100

    Add message in lesson6 branch

--- 1.py
+++ 1.py
@@ -6,8 +6,8 @@ driver = webdriver.Chrome()
 driver.get("http://uitestingplayground.com/ajax")
 driver.implicitly_wait(16)
 driver.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
-content = driver.find_element(By.CSS_SELECTOR, "#content")
-txt = content.find_element(By.CSS_SELECTOR, "p.bg-success").text
+content1 = driver.find_element(By.CSS_SELECTOR, "#content")
+txt = content1.find_element(By.CSS_SELECTOR, "p.bg-success").text
 print(txt)
 
 driver.quit()
